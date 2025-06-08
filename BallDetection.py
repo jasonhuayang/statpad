526aaf9faa75f933d6d1275d79042f6d97c2dbee
Jason Yang <jasonyang@macbookair.mynetworksettings.com>
2025-06-08 13:42:16 -0400

    not saving file anymore

--- BallDetection.py
+++ BallDetection.py
@@ -1,7 +1,30 @@
 from ultralytics import YOLO
 import cv2
 
-video_file = "test_clip.mp4"  # replace with your video file path
+video_file = "samples/test_clip.mp4"  # replace with your video file path
 model = YOLO('training/weights/best.pt')
-result = model.track(video_file, conf=0.2, persist=True, save=True, tracker="ball_tracker.yaml")
-print(result)
+cap = cv2.VideoCapture(video_file)
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLO11 tracking on the frame, persisting tracks between frames
+        results = model.track(frame, conf=0.2, persist=True, tracker="ball_tracker.yaml")
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLO11 Tracking", annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
--- BallDetectionSythetic.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-import cv2
-
-video_file = "test_clip.mp4"  # replace with your video file path
-model = YOLO('training/weights/best.pt')
-result = model.track(video_file, conf=0.2, persist=True, save=True, tracker="ball_tracker.yaml")
-print(result)
