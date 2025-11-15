c69bdf95839db14f72fc61f0185bb4418f63c57a
Jason Yang <jasonyang@Jasons-MacBook-Air.local>
2025-11-15 13:48:16 -0500

    added canny edge detection to court.py

--- Court.py
+++ Court.py
@@ -5,33 +5,24 @@ import cv2
 # define the video file to use for inference
 video_file = "samples/point2.mp4"  # Replace with your video file path
 cap = cv2.VideoCapture(video_file)
+def preprocess_frame(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    processed_frame = cv2.Canny(gray, 50, 150)
+    processed_frame = cv2.GaussianBlur(processed_frame,(5,5),0)
+    return processed_frame
 
-# load a pre-trained yolov8n model
-model = get_model(model_id="court-qo45v-lpwgg/1", api_key="Iuzy6U3O9RSmPobUEquD")
-
-# create supervision annotators
-
-edge_annotator = sv.EdgeAnnotator()
-
-# Process video frames
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
-    key_points = sv.KeyPoints(frame)
-    edge_annotator = sv.EdgeAnnotator(
-        color=sv.Color.GREEN,
-        thickness=5
-    )
-    annotated_frame = edge_annotator.annotate(
-        scene=frame.copy(),
-        key_points=key_points
-    )
-    # cv2.imshow("Processed Video", annotated_frame)
-    sv.plot_image(annotated_frame)
-    # Break the loop if 'q' is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+# Read the first frame
+ret, frame = cap.read()
+if ret:
+    processed_frame = preprocess_frame(frame)
+    
+    # Save the processed frame to disk
+    output_filename = "canny_first_frame_blurred.png"
+    cv2.imwrite(output_filename, processed_frame)
+    
+    sv.plot_image(processed_frame)
+else:
+    print("Error: Could not read the first frame from the video file")
 
 # Release resources
 cap.release()
